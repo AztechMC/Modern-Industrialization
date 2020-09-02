6347ca93d311cd9662040cd75c6528cd70ffe890
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2020-09-02 17:11:14 +0200

    Add first part of guide book

--- src/main/java/aztech/modern_industrialization/material/MaterialBlock.java
+++ src/main/java/aztech/modern_industrialization/material/MaterialBlock.java
@@ -2,12 +2,21 @@ package aztech.modern_industrialization.material;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import vazkii.patchouli.common.item.PatchouliItems;
 
 public class MaterialBlock extends Block {
 
@@ -17,6 +26,23 @@ public class MaterialBlock extends Block {
         super(settings);
         this.materialId = materialId;
         this.blockType = blockType;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack handStack = player.inventory.getMainHandStack();
+        if(handStack.getItem() == Items.BOOK && blockType == "ore") {
+            if(FabricLoader.getInstance().isModLoaded("patchouli")) {
+                ItemStack guide = new ItemStack(PatchouliItems.book);
+                CompoundTag tag = new CompoundTag();
+                tag.putString("patchouli:book", "modern_industrialization:book");
+                guide.setTag(tag);
+                player.inventory.offerOrDrop(world, guide);
+                handStack.decrement(1);
+                return ActionResult.success(world.isClient);
+            }
+        }
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 
     @Environment(EnvType.CLIENT)

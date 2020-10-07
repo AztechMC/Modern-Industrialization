c9abe4853d51c4b91bcb93049d0dfb3d45a2dbfe
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2020-10-07 15:08:04 +0200

    Add carbon dust usages

--- src/main/java/aztech/modern_industrialization/ModernIndustrialization.java
+++ src/main/java/aztech/modern_industrialization/ModernIndustrialization.java
@@ -263,6 +263,7 @@ public class ModernIndustrialization implements ModInitializer {
         FuelRegistry.INSTANCE.add(MIMaterials.coal.getItem("dust"), 1600);
         FuelRegistry.INSTANCE.add(MIMaterials.coal.getItem("tiny_dust"), 160);
         FuelRegistry.INSTANCE.add(MIMaterials.lignite_coal.getItem("lignite_coal"), 1600);
+        FuelRegistry.INSTANCE.add(MIItem.ITEM_CARBON_DUST, 3200);
 
         FluidFuelRegistry.register(MIFluids.CRUDE_OIL.key, 2);
         FluidFuelRegistry.register(MIFluids.DIESEL.key, 50);

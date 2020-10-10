fa8058ef1436c2e1f8f119791c24245cf1c87f9e
azercoco <corentin10simon@gmail.com>
2020-10-10 19:17:15 +0200

    Start implementing nuclear reactor + small fix

---
+++ src/main/java/aztech/modern_industrialization/MINuclearItem.java
@@ -0,0 +1,4 @@
+package aztech.modern_industrialization.util;
+
+public class MINuclearItem {
+}
---
+++ src/main/java/aztech/modern_industrialization/machines/nuclear/NuclearReactorBlockEntity.java
@@ -0,0 +1,4 @@
+package aztech.modern_industrialization.machines.nuclear;
+
+public class NuclearReactorBlockEntity {
+}

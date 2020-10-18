065e97c95b217b59ee75a604ddb7bd172fdbf5c1
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2020-10-18 12:02:48 +0200

    Enforce MIT License in every file

--- LICENSE.java
+++ LICENSE.java
@@ -21,23 +21,3 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package aztech.modern_industrialization.api.energy;
-
-public interface EnergyInsertable {
-    /**
-     * Attempt to partially insert energy into the block.
-     * 
-     * @param amount How much energy to insert.
-     * @return The leftover energy.
-     */
-    long insertEnergy(long amount);
-
-    /**
-     * Return whether the machine can accept energy from a given pipe tier.
-     * 
-     * @param tier The tier of the connexion.
-     * @return whether true if the machine can accept energy from that pipe tier, or
-     *         false otherwise.
-     */
-    boolean canInsert(CableTier tier);
-}

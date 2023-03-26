29e82e7724740d8abdd1a94cc6a6d2e65ac7f45f
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-03-27 00:14:23 +0200

    Use `me.jellysquid.mods` package for Sodium compat to fix animated textures

--- src/client/java/aztech/modern_industrialization/compat/sodium/SodiumCompat.java
+++ src/client/java/aztech/modern_industrialization/compat/sodium/SodiumCompat.java
@@ -39,7 +39,7 @@ public class SodiumCompat {
         MethodHandle handle = null;
         try {
             handle = MethodHandles.lookup().findStatic(
-                    Class.forName("net.caffeinemc.sodium.render.texture.SpriteUtil"),
+                    Class.forName("me.jellysquid.mods.sodium.client.render.texture.SpriteUtil"),
                     "markSpriteActive",
                     MethodType.methodType(void.class, TextureAtlasSprite.class));
             ModernIndustrialization.LOGGER.info("Loaded Modern Industrialization Sodium active sprite compat.");

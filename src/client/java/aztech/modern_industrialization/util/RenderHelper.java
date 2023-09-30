6088619d6bb6b1e0fe340ad8ea6ef49f6399726f
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-09-30 08:59:47 +0200

    Fix #571: sprites not animating in GUIs with Sodium

--- src/client/java/aztech/modern_industrialization/util/RenderHelper.java
+++ src/client/java/aztech/modern_industrialization/util/RenderHelper.java
@@ -223,6 +223,8 @@ public class RenderHelper {
         BufferUploader.drawWithShader(bufferBuilder.end());
 
         RenderSystem.enableDepthTest();
+
+        SodiumCompat.markSpriteActive(sprite);
     }
 
     /**

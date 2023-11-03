202acd262a88005cf7085858519ba8438edc1cc3
Justin Hu <justin.hu@alumni.ubc.ca>
2023-11-02 17:44:38 -0700

    fix license headers

--- src/client/java/aztech/modern_industrialization/blocks/toolstation/ToolStationScreen.java
+++ src/client/java/aztech/modern_industrialization/blocks/toolstation/ToolStationScreen.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Justin Hu
+ * Copyright (c) 2020 Azercoco & Technici4n
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

58a92b074725e14e2a71f4c206779544132ac19a
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-12-24 21:08:02 +0100

    Add creative tab back

--- src/main/java/aztech/modern_industrialization/MIRegistries.java
+++ src/main/java/aztech/modern_industrialization/MIRegistries.java
@@ -3,7 +3,10 @@ package aztech.modern_industrialization;
 import aztech.modern_industrialization.machines.gui.MachineMenuCommon;
 import aztech.modern_industrialization.proxy.CommonProxy;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.material.Fluid;
 import net.neoforged.bus.api.IEventBus;
@@ -12,6 +15,7 @@ import net.neoforged.neoforge.fluids.FluidType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
+import java.util.Comparator;
 import java.util.function.Supplier;
 
 /**
@@ -27,8 +31,23 @@ public class MIRegistries {
         return IMenuTypeExtension.create(CommonProxy.INSTANCE::createClientMachineMenu);
     })::get;
 
+    private static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MI.ID);
+
+    private static final Supplier<CreativeModeTab> TAB = TABS.register("general", () -> CreativeModeTab.builder()
+            .title(MIText.ModernIndustrialization.text())
+            .icon(() -> Items.IRON_BLOCK.getDefaultInstance())
+            // TODO NEO restore forge hammer tab icon
+//            .icon(() -> MIBlock.FORGE_HAMMER.asItem().getDefaultInstance())
+            .displayItems((params, output) -> {
+                MIItem.ITEM_DEFINITIONS.values().stream()
+                        .sorted(Comparator.comparing(e -> e.sortOrder))
+                        .forEach(output::accept);
+            })
+            .build());
+
     static void init(IEventBus modBus) {
         BLOCK_ENTITIES.register(modBus);
         MENUS.register(modBus);
+        TABS.register(modBus);
     }
 }

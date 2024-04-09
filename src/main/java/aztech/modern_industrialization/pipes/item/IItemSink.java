ef596bbca07686c02f969c9c70e7c1dc74845abb
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-04-09 15:26:41 +0200

    Refactor item pipe logic a bit to reduce item voiding chances

--- src/main/java/aztech/modern_industrialization/pipes/item/IItemSink.java
+++ src/main/java/aztech/modern_industrialization/pipes/item/IItemSink.java
@@ -23,35 +23,85 @@
  */
 package aztech.modern_industrialization.pipes.item;
 
+import aztech.modern_industrialization.MI;
 import java.util.List;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.server.level.ServerLevel;
 import net.neoforged.neoforge.items.IItemHandler;
 import net.neoforged.neoforge.items.ItemHandlerHelper;
 
-public interface IItemSink {
+interface IItemSink {
+    static int listMoveAll(List<? extends IItemSink> sinks, ServerLevel world, ExtractionSource target, int sourceSlot, int maxAmount) {
+        int moved = 0;
+
+        for (var sink : sinks) {
+            moved += sink.moveAll(world, target, sourceSlot, maxAmount - moved);
+            if (moved >= maxAmount) {
+                break;
+            }
+        }
+
+        return moved;
+    }
+
     /**
-     * @return leftover items
-     * @see IItemHandler#insertItem(int, ItemStack, boolean)
+     * Moves as many items as possible from an extraction target to this sink.
+     *
+     * @param world      current level, for logging in case of errors
+     * @param source     item source
+     * @param sourceSlot slot in the source to move from
+     * @param maxAmount  maximum amount of items to move
+     * @return the amount of items moved
      */
-    ItemStack insertItem(ItemStack stack, boolean simulate);
+    int moveAll(ServerLevel world, ExtractionSource source, int sourceSlot, int maxAmount);
 
     record HandlerWrapper(IItemHandler handler) implements IItemSink {
         @Override
-        public ItemStack insertItem(ItemStack stack, boolean simulate) {
-            return ItemHandlerHelper.insertItemStacked(handler, stack, simulate);
-        }
-    }
+        public int moveAll(ServerLevel world, ExtractionSource source, int sourceSlot, int maxToMove) {
+            IItemHandler sourceHandler = source.storage();
+            int moved = 0;
 
-    record Combined(List<? extends IItemSink> sinks) implements IItemSink {
-        @Override
-        public ItemStack insertItem(ItemStack stack, boolean simulate) {
-            for (IItemSink sink : sinks) {
-                stack = sink.insertItem(stack, simulate);
-                if (stack.isEmpty()) {
-                    return ItemStack.EMPTY;
+            // Repeated extraction because extractItem limits to max stack size
+            while (true) {
+                // Simulate first
+                var available = sourceHandler.extractItem(sourceSlot, maxToMove - moved, true);
+                if (available.isEmpty()) {
+                    break;
+                }
+
+                int availableCount = available.getCount();
+                var simulateLeftover = ItemHandlerHelper.insertItemStacked(handler, available, true);
+                int canFit = availableCount - simulateLeftover.getCount();
+                if (canFit <= 0) {
+                    break;
+                }
+
+                // Do!
+                var taken = sourceHandler.extractItem(sourceSlot, canFit, false);
+                int takenCount = taken.getCount();
+                var leftover = ItemHandlerHelper.insertItemStacked(handler, taken, false);
+
+                int movedThisTime = takenCount - leftover.getCount();
+                moved += movedThisTime;
+
+                if (!leftover.isEmpty()) {
+                    // Be nice and try to give the overflow back
+                    leftover = sourceHandler.insertItem(sourceSlot, leftover, false);
+
+                    // rest in pieces, little stacks
+                    if (!leftover.isEmpty()) {
+                        MI.LOGGER.error("Discarding overflowing item {}, extracted from block at position {} in {}, accessed from {} side",
+                                leftover, source.queryPos(), world.dimension(), source.querySide());
+                    }
+
+                    break;
+                }
+
+                if (movedThisTime == 0) {
+                    break;
                 }
             }
-            return stack;
+
+            return moved;
         }
     }
 }

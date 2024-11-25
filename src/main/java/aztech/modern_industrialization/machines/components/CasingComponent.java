07da29e64d5dfe50b379a17869e73e3907e08d6d
Swedz <me@swedz.net>
2024-11-24 19:59:06 -0500

    Make CableTier more accessible

--- src/main/java/aztech/modern_industrialization/machines/components/CasingComponent.java
+++ src/main/java/aztech/modern_industrialization/machines/components/CasingComponent.java
@@ -24,6 +24,7 @@
 package aztech.modern_industrialization.machines.components;
 
 import aztech.modern_industrialization.api.energy.CableTier;
+import aztech.modern_industrialization.api.energy.CableTierHolder;
 import aztech.modern_industrialization.machines.IComponent;
 import aztech.modern_industrialization.machines.MachineBlockEntity;
 import aztech.modern_industrialization.machines.models.MachineCasing;
@@ -42,15 +43,15 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.Nullable;
 
-public class CasingComponent implements IComponent, DropableComponent {
+public class CasingComponent implements IComponent, DropableComponent, CableTierHolder {
 
-    private ItemStack casingStack = ItemStack.EMPTY;
-    private CableTier currentTier = CableTier.LV;
+    protected ItemStack casingStack = ItemStack.EMPTY;
+    protected CableTier currentTier = CableTier.LV;
 
     /**
      * Sets the current casing stack and update {@link #currentTier} accordingly.
      */
-    private void setCasingStack(ItemStack stack) {
+    protected void setCasingStack(ItemStack stack) {
         casingStack = stack;
 
         // Compute tier
@@ -78,6 +79,11 @@ public class CasingComponent implements IComponent, DropableComponent {
     @Override
     public void readClientNbt(CompoundTag tag, HolderLookup.Provider registries) {
         currentTier = CableTier.getTier(tag.getString("casing"));
+    }
+
+    @Override
+    public CableTier getCableTier() {
+        return currentTier;
     }
 
     public void dropCasing(Level world, BlockPos pos) {
@@ -109,7 +115,7 @@ public class CasingComponent implements IComponent, DropableComponent {
         return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 
-    private void playCasingPlaceSound(MachineBlockEntity be) {
+    protected void playCasingPlaceSound(MachineBlockEntity be) {
         var blockKey = currentTier.itemKey;
         if (blockKey == null) {
             return; // no sound for LV

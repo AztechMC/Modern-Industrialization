e066f59006089992a7738be7fd2401e25b6b4fa5
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-11-06 23:20:23 +0100

    Fix #1219: Make automatic trash can able to void energy

--- src/main/java/aztech/modern_industrialization/blocks/TrashCanBlock.java
+++ src/main/java/aztech/modern_industrialization/blocks/TrashCanBlock.java
@@ -25,6 +25,9 @@
 package aztech.modern_industrialization.blocks;
 
 import aztech.modern_industrialization.MICapabilities;
+import aztech.modern_industrialization.api.energy.CableTier;
+import aztech.modern_industrialization.api.energy.EnergyApi;
+import aztech.modern_industrialization.api.energy.MIEnergyStorage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -145,12 +148,52 @@ public class TrashCanBlock extends Block {
         }
     }
 
+    private static class TrashEnergyStorage implements MIEnergyStorage {
+        private static final TrashEnergyStorage INSTANCE = new TrashEnergyStorage();
+
+        @Override
+        public boolean canConnect(CableTier cableTier) {
+            return true;
+        }
+
+        @Override
+        public long receive(long maxReceive, boolean simulate) {
+            return maxReceive;
+        }
+
+        @Override
+        public long extract(long maxExtract, boolean simulate) {
+            return 0;
+        }
+
+        @Override
+        public long getAmount() {
+            return 0;
+        }
+
+        @Override
+        public long getCapacity() {
+            return Long.MAX_VALUE;
+        }
+
+        @Override
+        public boolean canExtract() {
+            return false;
+        }
+
+        @Override
+        public boolean canReceive() {
+            return true;
+        }
+    }
+
     public static void onRegister(Block block, Item blockItem) {
         MICapabilities.onEvent(event -> {
             event.registerBlock(Capabilities.ItemHandler.BLOCK, (level, pos, state, be, direction) -> TrashItemHandler.INSTANCE, block);
             event.registerItem(Capabilities.ItemHandler.ITEM, (stack, ctx) -> TrashItemHandler.INSTANCE, blockItem);
             event.registerBlock(Capabilities.FluidHandler.BLOCK, (level, pos, state, be, direction) -> TrashFluidHandler.INSTANCE, block);
             event.registerItem(Capabilities.FluidHandler.ITEM, (stack, ctx) -> new TrashFluidHandlerItem(stack), blockItem);
+            event.registerBlock(EnergyApi.SIDED, (level, pos, state, be, direction) -> TrashEnergyStorage.INSTANCE, block);
         });
     }
 }

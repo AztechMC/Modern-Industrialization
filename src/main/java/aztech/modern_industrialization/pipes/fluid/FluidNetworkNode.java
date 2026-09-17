071f2be8635a7e9519519940f99665898ab85318
fmbellomy <fmbellomy@quantumgarbage.com>
2026-09-17 18:12:39 -0500

    add missed @Override

--- src/main/java/aztech/modern_industrialization/pipes/fluid/FluidNetworkNode.java
+++ src/main/java/aztech/modern_industrialization/pipes/fluid/FluidNetworkNode.java
@@ -266,7 +266,7 @@ public class FluidNetworkNode extends PipeNetworkNode {
                 pipe.sync();
             }
         }
-
+        @Override
         public Direction getDirection() {
             return direction;
         }

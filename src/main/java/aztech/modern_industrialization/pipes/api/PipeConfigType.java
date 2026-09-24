7e9b39cf9252ef6ee050f38267760dff58c43938
fmbellomy <fmbellomy@quantumgarbage.com>
2026-09-23 19:40:57 -0500

    use StringRepresentable for the PipeConfigType codec

--- src/main/java/aztech/modern_industrialization/pipes/api/PipeConfigType.java
+++ src/main/java/aztech/modern_industrialization/pipes/api/PipeConfigType.java
@@ -25,37 +25,22 @@
 package aztech.modern_industrialization.pipes.api;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
+import net.minecraft.util.StringRepresentable;
 
-public enum PipeConfigType {
-    NONE(0),
-    ITEM(1),
-    FLUID(2);
+public enum PipeConfigType implements StringRepresentable {
+    NONE("NONE"),
+    ITEM("ITEM"),
+    FLUID("FLUID");
 
-    private final int id;
+    private final String id;
 
-    PipeConfigType(int id) {
+    PipeConfigType(String id) {
         this.id = id;
     }
 
-    public int getId() {
+    public String getSerializedName() {
         return id;
     }
 
-    public static PipeConfigType decodeConnectionType(int i) {
-        return i == 0 ? NONE : i == 1 ? ITEM : FLUID;
-    }
-
-    public static int encodeConnectionType(PipeConfigType config) {
-        return config == NONE ? 0 : config == ITEM ? 1 : 2;
-    }
-
-    public static final Codec<PipeConfigType> CODEC = Codec.INT.comapFlatMap(
-            i -> switch (i) {
-                case 0 -> DataResult.success(NONE);
-                case 1 -> DataResult.success(ITEM);
-                case 2 -> DataResult.success(FLUID);
-                default -> DataResult.error(() -> "Unknown pipe connection type: " + i);
-            },
-            PipeConfigType::encodeConnectionType);
+    public static final Codec<PipeConfigType> CODEC = StringRepresentable.fromEnum(PipeConfigType::values);
 }
